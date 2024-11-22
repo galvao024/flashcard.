5614cd4d077df02c8599c1d848f6343562cf9495
galvao024 <146440706+galvao024@users.noreply.github.com>
2024-11-21 23:47:42 -0300

    Update question.js

--- question.js
+++ question.js
@@ -1,22 +1,23 @@
-(
-    'Matemática',
-    'Quanto é 8x8x4?',
-    '256'
-)
-criaCartao
 criaCartao(
-    'História',
-    'Em que ano o Brasil foi descoberto?',
-    '1500'
+    'Programação',
+    'O que é Lua?',
+    'Lua é uma linguagem de programação'
 )
-
-criaCartao(
-    'Ciências',
-    'Qual é o maior planeta do sistema solar?',
-    'Júpiter '
 
 criaCartao(
     'Geografia',
-    'Qual é o país mais extenso do mundo?',
-    'Rússia '
+    'Qual a capital da Finlândia?',
+    'A capital da Finlândia é Helsinque'
+)
+
+criaCartao(
+    'Culinária',
+    'Quais são os pratos típicos da Itália?',
+    'Os pratos mais típicos da Itália são: Pizza,Ravioli, Spaghetti, Lasagna, Risotto e Arancino'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz romã em Inglês?',
+    'Romã em ingles é pomegranate'
 )
