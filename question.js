d2c010ebfda9f0a6afdbfe396dbf7eaab99415aa
galvao024 <146440706+galvao024@users.noreply.github.com>
2024-11-22 02:28:25 +0000

    .

--- question.js
+++ question.js
@@ -5,19 +5,18 @@
 )
 criaCartao
 criaCartao(
+    'História',
+    'Em que ano o Brasil foi descoberto?',
+    '1500'
+)
+
+criaCartao(
+    'Ciências',
+    'Qual é o maior planeta do sistema solar?',
+    'Júpiter '
+
+criaCartao(
     'Geografia',
-    'Qual a capital da Finlândia?',
-    'A capital da Finlândia é Helsinque'
-)
-
-criaCartao(
-    'Culinária',
-    'Quais são os pratos típicos da Itália?',
-    'Os pratos mais típicos da Itália são: Pizza,Ravioli, Spaghetti, Lasagna, Risotto e Arancino'
-)
-
-criaCartao(
-    'Lingua inglesa',
-    'Como se diz romã em Inglês?',
-    'Romã em ingles é pomegranate'
+    'Qual é o país mais extenso do mundo?',
+    'Rússia '
 )
